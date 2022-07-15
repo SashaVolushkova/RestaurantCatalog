7ad7f6c9d4548a61b1d2cd165161dfbcc86353a8
Alexandra <sobakasasha114@gmail.com>
2022-07-16 02:37:13 +0300

    add test controller

---
+++ src/main/java/org/example/restaurant/Restaurant.java
@@ -0,0 +1,11 @@
+package org.example.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Restaurant {
+    public static void main(String[] args) {
+        SpringApplication.run(Restaurant.class, args);
+    }
+}
