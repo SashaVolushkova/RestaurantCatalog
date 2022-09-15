7b00294b6d8f2e23b0a23140d702fee364f33aed
Alexandra <sobakasasha114@gmail.com>
2022-09-15 15:36:54 +0300

    fix

--- src/main/java/org/example/restaurant/model/ReviewEntity.java
+++ src/main/java/org/example/restaurant/model/ReviewEntity.java
@@ -1,8 +1,13 @@
 package org.example.restaurant.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Objects;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "review")
 public class ReviewEntity {
@@ -28,51 +33,5 @@ public class ReviewEntity {
         this.restaurant = restaurantEntity;
         this.reviewText = text;
         this.rating = rate;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public RestaurantEntity getRestaurant() {
-        return restaurant;
-    }
-
-    public void setRestaurant(RestaurantEntity restaurant) {
-        this.restaurant = restaurant;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
-
-    public String getReviewText() {
-        return reviewText;
-    }
-
-    public void setReviewText(String reviewText) {
-        this.reviewText = reviewText;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ReviewEntity that = (ReviewEntity) o;
-        return Objects.equals(id, that.id) && Objects.equals(restaurant.getId(),that.restaurant.getId())
-                && Objects.equals(rating, that.rating) && Objects.equals(reviewText, that.reviewText);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, restaurant, rating, reviewText);
     }
 }
