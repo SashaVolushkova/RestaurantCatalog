c970a5d89e4237d4b26b696a94acd9e2d3579a7b
alexandra.volushkova <alexandra.volushkova@ax-team.com>
2022-09-30 17:53:13 +0300

    remove logs remove imports

--- src/test/java/org/example/restaurant/util/AppContextTest.java
+++ src/test/java/org/example/restaurant/util/AppContextTest.java
@@ -3,7 +3,6 @@ package org.example.restaurant.util;
 import org.example.restaurant.RestaurantApp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = {
